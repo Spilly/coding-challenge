6257cd0650e9d7e67b84c5c223bb535c800cbd35
Aaron <amcdanie@gmail.com>
2022-08-15 15:46:05 -0700

    Remove Pods directory

--- Pods/Target Support Files/Pods-CodingChallenge-CodingChallengeUITests/Pods-CodingChallenge-CodingChallengeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CodingChallenge_CodingChallengeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CodingChallenge_CodingChallengeUITests
-@end
--- Pods/Target Support Files/Pods-CodingChallengeTests/Pods-CodingChallengeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CodingChallengeTests : NSObject
-@end
-@implementation PodsDummy_Pods_CodingChallengeTests
-@end
